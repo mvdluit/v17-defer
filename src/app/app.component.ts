6fcb11df771a8e41493fb7108469c44bb7a2b8d7
Michael van der Luit <3525053+mvdluit@users.noreply.github.com>
2023-11-29 02:49:56 +0100

    fix formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,21 +19,27 @@ import { TwoComponent } from './two/two.component';
     <h1>Welcome to {{ title }}!</h1>
 
     @defer (on viewport(myPlaceholderOne)) {
-    <app-one />
+      <app-one />
     } @placeholder(minimum 5000ms) {
-    <p class="bg one" #myPlaceholderOne>I'm a placeholder for one</p>
-    } @defer (on viewport(myPlaceholderTwo)) {
-    <app-two />
+      <p class="bg one" #myPlaceholderOne>I'm a placeholder for one</p>
+    }
+
+    @defer (on viewport(myPlaceholderTwo)) {
+      <app-two />
     } @placeholder(minimum 5000ms) {
-    <p class="bg two" #myPlaceholderTwo>I'm a placeholder for two</p>
-    } @defer (on viewport(myPlaceholderThree)) {
-    <app-three />
+      <p class="bg two" #myPlaceholderTwo>I'm a placeholder for two</p>
+    }
+
+    @defer (on viewport(myPlaceholderThree)) {
+      <app-three />
     } @placeholder(minimum 5000ms) {
-    <p class="bg three" #myPlaceholderThree>I'm a placeholder for three</p>
-    } @defer (on viewport(myPlaceholderFour)) {
-    <app-four />
+      <p class="bg three" #myPlaceholderThree>I'm a placeholder for three</p>
+    }
+
+    @defer (on viewport(myPlaceholderFour)) {
+      <app-four />
     } @placeholder(minimum 5000ms) {
-    <p class="bg four" #myPlaceholderFour>I'm a placeholder for four</p>
+      <p class="bg four" #myPlaceholderFour>I'm a placeholder for four</p>
     }
   `,
   styles: [],
